d31e618509de8ce6c882e972dfa77e7cdfcfe5f0
Udi Trugman <udi@trugman.com>
2021-12-30 07:26:57 +0200

    a

--- feedback/lib/src/feedback_widget.dart
+++ feedback/lib/src/feedback_widget.dart
@@ -154,6 +154,7 @@ class FeedbackWidgetState extends State<FeedbackWidget>
                   ),
                 ),
               ),
+              if (widget.isFeedbackVisible || !animation.isDismissed)
               Align(
                 alignment: Alignment(controlsHorizontalAlignment.value, -0.7),
                 child: ControlsColumn(
